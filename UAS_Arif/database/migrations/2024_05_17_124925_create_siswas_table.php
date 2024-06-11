d02985004a945fcd2064db3ad73b96d02426308e
Necma13 <145630799+Necma13@users.noreply.github.com>
2024-06-10 23:06:27 -0700

    membuat database

---
+++ UAS_Arif/database/migrations/2024_05_17_124925_create_siswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->string('nisn')->primary();
+            $table->string('nama');
+            $table->date('tgl');
+            $table->string('alamat');
+            $table->string('jjg');
+            $table->string('hp');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
---
+++ UAS_Arif/database/migrations/2024_05_17_125030_create_pembayarans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nofak')->unique();
+            $table->string('nisn')->unique();
+            $table->dateTime('tgl_b');
+            $table->integer('jum_b');
+            $table->string('bukti_b');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
